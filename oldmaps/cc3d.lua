b741c7da6361445610fb36ef59b82c92d7b50a17
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2025-04-29 21:38:26 +0100

    Create cc3d.lua

---
+++ oldmaps/cc3d.lua
@@ -0,0 +1,154 @@
+return {
+    {
+        {
+            {
+                {
+                    OffsetStudsU = 0,
+                    OffsetStudsV = 0,
+                    Face = [==[Top]==],
+                    StudsPerTileU = 52,
+                    Transparency = 0,
+                    Color3 = Color3.new(1, 1, 1),
+                    ClassName = [==[Texture]==],
+                    Name = [==[Texture]==],
+                    StudsPerTileV = 52,
+                    Texture = [==[rbxassetid://102886451449612]==]
+                },
+                RightParamA = -0.5,
+                Reflectance = 0,
+                Color = Color3.new(0.356863, 0.356863, 0.356863),
+                Anchored = true,
+                RightSurface = [==[Smooth]==],
+                RotVelocity = Vector3.new(0, 0, 0),
+                CFrame = CFrame.new(-55.3289986, -1.01600003, -117.341003, 1, -0, 0, 0, 1, 0, -0, 0, 1),
+                Material = [==[Plastic]==],
+                FrontSurface = [==[Smooth]==],
+                Size = Vector3.new(624.4299926757812, 1, 635.9639892578125),
+                Friction = 0.30000001192092896,
+                BackSurface = [==[Smooth]==],
+                RightSurfaceInput = [==[NoInput]==],
+                CanCollide = true,
+                LeftSurfaceInput = [==[NoInput]==],
+                BottomSurface = [==[Smooth]==],
+                Velocity = Vector3.new(0, 0, 0),
+                LeftSurface = [==[Smooth]==],
+                Elasticity = 0.5,
+                Transparency = 0,
+                Name = [==[Baseplate]==],
+                ClassName = [==[Part]==],
+                Orientation = Vector3.new(0, 0, 0),
+                LeftParamA = -0.5,
+                Shape = [==[Block]==],
+                TopSurface = [==[Smooth]==]
+            },
+            {
+                {
+                    {
+                        {
+                            RightParamA = -0.5,
+                            Reflectance = 0,
+                            Color = Color3.new(0.639216, 0.635294, 0.647059),
+                            Velocity = Vector3.new(0, 0, 0),
+                            RightSurface = [==[Smooth]==],
+                            RotVelocity = Vector3.new(0, 0, 0),
+                            Material = [==[Plastic]==],
+                            Size = Vector3.new(265.66400146484375, 6.980000019073486, 265.66400146484375),
+                            Friction = 0.30000001192092896,
+                            BackSurface = [==[Smooth]==],
+                            BottomSurface = [==[Smooth]==],
+                            CanCollide = true,
+                            FrontSurface = [==[Smooth]==],
+                            CFrame = CFrame.new(-7.90299988, 2.398, -48.269001, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+                            RightSurfaceInput = [==[NoInput]==],
+                            LeftSurface = [==[Smooth]==],
+                            Elasticity = 0.5,
+                            Transparency = 0,
+                            Name = [==[Cylinder]==],
+                            ClassName = [==[MeshPart]==],
+                            Orientation = Vector3.new(0, 0, 0),
+                            LeftParamA = -0.5,
+                            LeftSurfaceInput = [==[NoInput]==],
+                            TopSurface = [==[Smooth]==]
+                        },
+                        Name = [==[Cylinder_Node]==],
+                        ClassName = [==[Model]==]
+                    },
+                    Name = [==[fencircular]==],
+                    ClassName = [==[Model]==]
+                },
+                Name = [==[fencircular]==],
+                ClassName = [==[Model]==]
+            },
+            {
+                {
+                    {
+                        {
+                            RightParamA = -0.5,
+                            Reflectance = 0,
+                            Color = Color3.new(0.639216, 0.635294, 0.647059),
+                            Velocity = Vector3.new(0, 0, 0),
+                            RightSurface = [==[Smooth]==],
+                            RotVelocity = Vector3.new(0, 0, 0),
+                            Material = [==[Plastic]==],
+                            Size = Vector3.new(37.0820198059082, 28.694122314453125, 20),
+                            Friction = 0.30000001192092896,
+                            BackSurface = [==[Smooth]==],
+                            BottomSurface = [==[Smooth]==],
+                            CanCollide = true,
+                            FrontSurface = [==[Smooth]==],
+                            CFrame = CFrame.new(-216.153427, 12.0050611, -163.672974, 0.60876137, 0, 0.793353379, 0, 1, 0, -0.793353379, 0, 0.60876137),
+                            RightSurfaceInput = [==[NoInput]==],
+                            LeftSurface = [==[Smooth]==],
+                            Elasticity = 0.5,
+                            Transparency = 0,
+                            Name = [==[Cube]==],
+                            ClassName = [==[MeshPart]==],
+                            Orientation = Vector3.new(0, 52.5, 0),
+                            LeftParamA = -0.5,
+                            LeftSurfaceInput = [==[NoInput]==],
+                            TopSurface = [==[Smooth]==]
+                        },
+                        Name = [==[Cube_Node]==],
+                        ClassName = [==[Model]==]
+                    },
+                    Name = [==[stonehouse]==],
+                    ClassName = [==[Model]==]
+                },
+                Name = [==[stonehouse]==],
+                ClassName = [==[Model]==]
+            },
+            Name = [==[Workspace]==],
+            ClassName = [==[Workspace]==]
+        },
+        {
+            {
+                StarCount = 3000,
+                SkyboxUp = [==[rbxassetid://135984804546013]==],
+                MoonTextureId = [==[rbxasset://sky/moon.jpg]==],
+                SkyboxBk = [==[rbxassetid://135984804546013]==],
+                ClassName = [==[Sky]==],
+                SkyboxFt = [==[rbxassetid://135984804546013]==],
+                SkyboxDn = [==[rbxassetid://135984804546013]==],
+                Name = [==[Sky]==],
+                SunTextureId = [==[rbxasset://sky/sun.jpg]==],
+                SunAngularSize = 21,
+                SkyboxLf = [==[rbxassetid://135984804546013]==],
+                SkyboxRt = [==[rbxassetid://135984804546013]==],
+                MoonAngularSize = 11
+            },
+            Name = [==[Lighting]==],
+            ClassName = [==[Lighting]==]
+        },
+        {
+            Name = [==[Teams]==],
+            ClassName = [==[Teams]==]
+        },
+        {
+            Name = [==[Folder]==],
+            ClassName = [==[Folder]==]
+        },
+        {},
+        Name = [==[Game]==],
+        ClassName = [==[DataModel]==]
+    }
+}
